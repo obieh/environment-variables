f8f7e9ff497c052810ee859617037490e66cf7cd
obieh <o_amadic@yahoo.com>
2025-09-25 00:13:39 +0100

    hard param

---
+++ aws_cloud_manager.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Checking and acting on the environment variable
+if [ "$ENVIRONMENT" == "local" ]; then
+echo "Running script for Local Environment..."
+# Commands for local environment
+elif [ "$ENVIRONMENT" == "testing" ]; then
+echo "Running script for Testing Environment..."
+# Commands for testing environment
+elif [ "$ENVIRONMENT" == "production" ]; then
+echo "Running script for Production Environment..."
+# Commands for production environment
+else
+echo "No environment specified or recognized."
+exit 2
+fi
