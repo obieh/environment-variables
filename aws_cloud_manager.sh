809ac850915c2bdd2bf4e901bf372a18dbd5e9e8
obieh <o_amadic@yahoo.com>
2025-09-25 11:40:47 +0100

    updated script

--- aws_cloud_manager.sh
+++ aws_cloud_manager.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-# Checking and acting on the environment variable
+# Checking the number of arguments
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <environment>"
+    exit 1
+fi
+
+# Accessing the first argument
+ENVIRONMENT=$1
+
+# Acting based on the argument value
 if [ "$ENVIRONMENT" == "local" ]; then
-echo "Running script for Local Environment..."
-# Commands for local environment
+  echo "Running script for Local Environment..."
 elif [ "$ENVIRONMENT" == "testing" ]; then
-echo "Running script for Testing Environment..."
-# Commands for testing environment
+  echo "Running script for Testing Environment..."
 elif [ "$ENVIRONMENT" == "production" ]; then
-echo "Running script for Production Environment..."
-# Commands for production environment
+  echo "Running script for Production Environment..."
 else
-echo "No environment specified or recognized."
-exit 2
+  echo "Invalid environment specified. Please use 'local', 'testing', or 'production'."
+  exit 2
 fi
